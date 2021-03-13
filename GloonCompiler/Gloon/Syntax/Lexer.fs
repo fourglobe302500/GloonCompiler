53552f2fa1b987bfbb795d60ef8eefddbb682cb2
fourglobe302500 <franciscomiguelolima@gmail.com>
2021-03-13 13:42:43 +0000

    Cleaning the Lexer

--- GloonCompiler/Gloon/Syntax/Lexer.fs
+++ GloonCompiler/Gloon/Syntax/Lexer.fs
@@ -3,13 +3,9 @@
 module internal Lexer =
 
   open System
-  open Gloon.Syntax
   open Gloon.Text
-
-  let private consume f current next constructor =
-    while f (current ()) do
-      next ()
-    constructor ()
+  open Gloon.Syntax
+  open Gloon.Syntax.Facts
 
   let Lex (s: string) =
     let mutable position = 0
@@ -25,8 +21,13 @@ module internal Lexer =
     let inline text start = s.[start..position - 1]
     let inline get amount = if position + amount >= s.Length then s.[position..s.Length-1] else s.[position..position+amount]
 
-    let inline move x = position <- position + x; position - x
-    let inline next () = move 1 |> ignore
+    let inline move x = position <- position + x
+    let inline next () = move 1
+
+    let consume f validator =
+      while f (current ()) do
+        next ()
+      validator ()
 
     let getKeywordKind = function
     | "true" -> BooleanLiteralToken true
@@ -40,46 +41,40 @@ module internal Lexer =
 
     let nextToken () : Token =
       let start = position
+      let mutable kind = EndOfFileToken
+      let mutable value = null
       match current () with
-      | _ when position >= s.Length -> {Position = move 1; Text = text start; Kind = EndOfFileToken; Value = null}
-      | n when Char.IsNumber n ->
-        consume Char.IsNumber current next (fun () ->
-          let res = ref 0
-          if not (Int32.TryParse (text start, res))
-          then
-            diagnostics.ReportInvallidNumber start (position - start) (text start)
-            {Position = start; Text = text start; Kind = NumberLiteralToken 0; Value = null}
-          else {Position = start; Text = text start; Kind = NumberLiteralToken res.Value; Value = res.Value})
-      | w when Char.IsWhiteSpace w ->
-        consume Char.IsWhiteSpace current next (fun () ->
-          {Position = start; Text = text start; Kind = WhiteSpaceToken (text start); Value = null})
-      | '+' when lookAhead () = '+' -> {Position = move 2; Text = text start; Kind = IncrementToken; Value = null}
-      | '+' -> {Position = move 1; Text = text start; Kind = PlusToken; Value = null}
-      | '-' when lookAhead () = '-' -> {Position = move 2; Text = text start; Kind = DecrementToken; Value = null}
-      | '-' -> {Position = move 1; Text = text start; Kind = MinusToken; Value = null}
-      | '*' when lookAhead () = '*' -> {Position = move 2; Text = text start; Kind = PowerToken; Value = null}
-      | '*' -> {Position = move 1; Text = text start; Kind = StarToken; Value = null}
-      | '/' when lookAhead () = '/' -> {Position = move 2; Text = text start; Kind = RootToken; Value = null}
-      | '/' -> {Position = move 1; Text = text start; Kind = SlashToken; Value = null}
-      | '(' -> {Position = move 1; Text = text start; Kind = OpenParenToken; Value = null}
-      | ')' -> {Position = move 1; Text = text start; Kind = CloseParenToken; Value = null}
-      | '%' -> {Position = move 1; Text = text start; Kind = PercentToken; Value = null}
-      | '&' when lookAhead () = '&' -> {Position = move 2; Text = text start; Kind = DoubleAmpersandToken; Value = null}
-      | '|' when lookAhead () = '|' -> {Position = move 2; Text = text start; Kind = DoublePipeToken; Value = null}
-      | '=' when lookAhead () = '=' -> {Position = move 2; Text = text start; Kind = DoubleEqualsToken; Value = null}
-      | '=' -> {Position = move 1; Text = text start; Kind = EqualsToken; Value = null}
-      | '!' when lookAhead () = '=' -> {Position = move 2; Text = text start; Kind = BangEqualsToken; Value = null}
-      | '!' -> {Position = move 1; Text = text start; Kind = BangToken; Value = null}
-      | '<' when lookAhead () = '=' -> {Position = move 2; Text = text start; Kind = LessThanEqualsToken; Value = null}
-      | '<' -> {Position = move 1; Text = text start; Kind = LessThanToken; Value = null}
-      | '>' when lookAhead () = '=' -> {Position = move 2; Text = text start; Kind = GreaterThanEqualsToken; Value = null}
-      | '>' -> {Position = move 1; Text = text start; Kind = GreaterThanToken; Value = null}
-      | l when Char.IsLetter l ->
-        consume Char.IsLetter current next (fun () ->
-          {Position = start; Text = text start; Kind = getKeywordKind (text start); Value = getKeywordValue (text start)})
-      | _ ->
-        diagnostics.ReportInvallidCharacter start (current ())
-        {Position = move 1; Text = (text start); Kind = InvallidToken (text start); Value = null}
+      | _ when position >= s.Length -> next ()
+      | '+' when lookAhead () = '+' ->  move 2; kind <- IncrementToken
+      | '+' ->                          move 1; kind <- PlusToken
+      | '-' when lookAhead () = '-' ->  move 2; kind <- DecrementToken
+      | '-' ->                          move 1; kind <- MinusToken
+      | '*' when lookAhead () = '*' ->  move 2; kind <- PowerToken
+      | '*' ->                          move 1; kind <- StarToken
+      | '/' when lookAhead () = '/' ->  move 2; kind <- RootToken
+      | '/' ->                          move 1; kind <- SlashToken
+      | '%' ->                          move 1; kind <- PercentToken
+      | '&' when lookAhead () = '&' ->  move 2; kind <- DoubleAmpersandToken
+      | '|' when lookAhead () = '|' ->  move 2; kind <- DoublePipeToken
+      | '=' when lookAhead () = '=' ->  move 2; kind <- DoubleEqualsToken
+      | '=' ->                          move 1; kind <- EqualsToken
+      | '!' when lookAhead () = '=' ->  move 2; kind <- BangEqualsToken
+      | '!' ->                          move 1; kind <- BangToken
+      | '<' when lookAhead () = '=' ->  move 2; kind <- LessThanEqualsToken
+      | '<' ->                          move 1; kind <- LessThanToken
+      | '>' when lookAhead () = '=' ->  move 2; kind <- GreaterThanEqualsToken
+      | '>' ->                          move 1; kind <- GreaterThanToken
+      | '(' ->                          move 1; kind <- OpenParenToken
+      | ')' ->                          move 1; kind <- CloseParenToken
+      | n when Char.IsNumber n -> consume Char.IsNumber <| fun () ->
+        let res = ref 0
+        if not (Int32.TryParse (text start, res))
+        then diagnostics.ReportInvallidNumber start (position - start) (text start); kind <- NumberLiteralToken 0
+        else kind <- NumberLiteralToken res.Value; value <- res.Value :> obj
+      | l when Char.IsLetter l -> consume Char.IsLetter <| fun () -> kind <- getKeywordKind (text start); value <- getKeywordValue (text start)
+      | w when Char.IsWhiteSpace w -> consume Char.IsWhiteSpace <| fun () -> kind <- WhiteSpaceToken (text start)
+      | _ -> diagnostics.ReportInvallidCharacter start (current ()); move 1; kind <- InvallidToken (text start)
+      { Position = start; Text = kind.Text; Kind = kind; Value = value}
     let mutable Break = false
     let tokens = ResizeArray ()
     while not Break do
